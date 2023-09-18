cdddcdd2a718390870a4f6603e96a14095c55333
Avish Malik <avishmalik8@gmail.com>
2023-09-19 02:06:25 +0530

    added dark mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Providers from './Providers'
 import Header from './components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -13,13 +14,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Header */}
-        <Header />
-        {/* Navbar */}
+        <Providers>
+          {/* Header */}
+          <Header />
+          {/* Navbar */}
 
-        {/* SearchBox */}
+          {/* SearchBox */}
 
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   )
